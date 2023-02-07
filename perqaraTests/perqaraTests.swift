d15fb03487b1759ad8a3477412b6c388383875ab
ikaru19 <syafrizalizal17@gmail.com>
2023-02-07 14:43:19 +0700

    Add Entity Unit Testing

--- perqaraTests/perqaraTests.swift
+++ perqaraTests/perqaraTests.swift
@@ -24,6 +24,9 @@ final class perqaraTests: XCTestCase {
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        test_entity_should_produce_game()
+        test_game_should_produce_different_entity()
+        test_entity_should_produce_data_game()
     }
 
     func testPerformanceExample() throws {
@@ -32,5 +35,55 @@ final class perqaraTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
+    
+    func test_entity_should_produce_game() {
+        let entity = Domain.GameEntity(
+            id: "1",
+            name: "name",
+            released: "released",
+            backgroundImage: "backgorundImage",
+            rating: "rating",
+            description: "description",
+            suggestionsCount: "suggestionsCount",
+            developer: "developer",
+            isFavorite: false
+        )
+        XCTAssert(entity != nil)
+    }
+    
+    func test_game_should_produce_different_entity() {
+        let entity = Domain.GameEntity(
+            id: "1",
+            name: "name",
+            released: "released",
+            backgroundImage: "backgorundImage",
+            rating: "rating",
+            description: "description",
+            suggestionsCount: "suggestionsCount",
+            developer: "developer",
+            isFavorite: false
+        )
+        var entity2 = entity
+        entity2.id = "2"
+        XCTAssert(entity.id != entity2.id)
+    }
 
+    func test_entity_should_produce_data_game() {
+        let developer = Data.DataDeveloperEntity(
+            id: 1,
+            name: "dev name",
+            backgroundImage: "test"
+        )
+        let entity = Data.DataGameEntity(
+            id: 1,
+            name: "name",
+            released: "released",
+            backgroundImage: "backgroundImage",
+            rating: 2.0,
+            description: "descritpion",
+            suggestionsCount: 4,
+            developer: [developer]
+        )
+        XCTAssert(entity != nil)
+    }
 }
